ecd8405f52855efc92de20c2235c555ad78c35ec
JuanDBta <juandavid_diaz@hotmail.com>
2023-05-08 23:48:47 -0500

    Luxon installed. Add Date below NavBar using Luxon. Project ready for Review

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 import { Books } from './modules/Books_class.js';
 import { updateDateTime } from './modules/display_time.js';
 import { addBookSubmit, removeBookClick } from './modules/add_remove_book.js';
@@ -13,3 +14,7 @@ addBookSubmit();
 removeBookClick();
 
 controlNavBar('#list', '#addBook', '#contact');
+
+const currentDateElement = document.getElementById('current-date');
+const currentDate = DateTime.local().toLocaleString(DateTime.DATE_FULL);
+currentDateElement.textContent = currentDate;
